90ab5f4383f2170bda334667e8e505fe24d34532
Yuvi <singhraj0461@gmail.com>
2024-07-13 10:10:15 +0530

    done with this

--- src/Api/questionApi.js
+++ src/Api/questionApi.js
@@ -1,32 +1,139 @@
 const question = [
     {
         id: 1,
-        question: "Capital of Bihar  is ?",
-        A: "Muzaffarpur",
-        B: "Motihari",
-        C: "Rohtas",
-        D: "Patna",
-        Ans: "Patna"
+        question: "What is at the core of Java?",
+        A: "Method",
+        B: "Variable",
+        C: "Class",
+        D: "Object",
+        Ans: "Class"
     },
     {
         id: 2,
-        question: "Capital of India ?",
-        A: "Kolkata",
-        B: "New Delhi",
-        C: "Delhi",
-        D: "Mumbai",
-        Ans: "New Delhi"
+        question: "What does a class define in Java?",
+        A: "Syntax",
+        B: "Object",
+        C: "Method",
+        D: "Template",
+        Ans: "Template"
     },
     {
         id: 3,
-        question: "WHO full form ?",
-        A: "World Health Organization",
-        B: "World hero Organization",
-        C: "Weak health Option ",
-        D: "World Help Organization ",
-        Ans: "World Health Organization"
+        question: "What is the new data type created by defining a class called?",
+        A: "Method",
+        B: "Object",
+        C: "Instance",
+        D: "Variable",
+        Ans: "Object"
     },
-
-]
+    {
+        id: 4,
+        question: "What keyword is used to declare a class?",
+        A: "public",
+        B: "void",
+        C: "class",
+        D: "new",
+        Ans: "class"
+    },
+    {
+        id: 5,
+        question: "What are the variables defined within a class called?",
+        A: "Local",
+        B: "Global",
+        C: "Static",
+        D: "Instance",
+        Ans: "Instance"
+    },
+    {
+        id: 6,
+        question: "What contains the code within a class?",
+        A: "Variables",
+        B: "Methods",
+        C: "Keywords",
+        D: "Instances",
+        Ans: "Methods"
+    },
+    {
+        id: 7,
+        question: "What operator is used to access instance variables?",
+        A: ":",
+        B: ";",
+        C: ".",
+        D: ",",
+        Ans: "."
+    },
+    {
+        id: 8,
+        question: "What method is used as the entry point of a Java program?",
+        A: "start()",
+        B: "main()",
+        C: "run()",
+        D: "init()",
+        Ans: "main()"
+    },
+    {
+        id: 9,
+        question: "Which keyword is used to create a new object in Java?",
+        A: "create",
+        B: "build",
+        C: "new",
+        D: "alloc",
+        Ans: "new"
+    },
+    {
+        id: 10,
+        question: "What do you call a physical copy of a class?",
+        A: "Template",
+        B: "Instance",
+        C: "Method",
+        D: "Variable",
+        Ans: "Instance"
+    },
+    {
+        id: 11,
+        question: "Which statement correctly creates an instance of the Box class?",
+        A: "Box mybox = Box();",
+        B: "Box mybox = new Box();",
+        C: "Box mybox = new Box;",
+        D: "Box mybox = Box.new();",
+        Ans: "Box mybox = new Box();"
+    },
+    {
+        id: 12,
+        question: "What do you call the members of a class collectively?",
+        A: "Methods",
+        B: "Variables",
+        C: "Instances",
+        D: "Members",
+        Ans: "Members"
+    },
+    {
+        id: 13,
+        question: "What type of variables does each instance of a class contain?",
+        A: "Local",
+        B: "Global",
+        C: "Static",
+        D: "Instance",
+        Ans: "Instance"
+    },
+    {
+        id: 14,
+        question: "What is the output of the program that declares two Box objects with different dimensions?",
+        A: "Volume is 3000.0",
+        B: "Volume is 162.0",
+        C: "Both A and B",
+        D: "None",
+        Ans: "Both A and B"
+    },
+    {
+        id: 15,
+        question: "What must be done to use a variable of a class type before assigning an object to it?",
+        A: "Define",
+        B: "Declare",
+        C: "Initialize",
+        D: "Assign",
+        Ans: "Declare"
+    }
+];
 
 export default question;
--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -27,62 +27,61 @@ function Question() {
   };
 
   return (
-    <>
-      {question.map((quets) => {
-        const { id, question, A, B, C, D, Ans } = quets;
+    <div className="container mx-auto p-4">
+      {question.map((quet) => {
+        const { id, question, A, B, C, D, Ans } = quet;
         const questionStatus = answeredQuestions[id];
 
         return (
           <div
-            className="text-white flex items-center justify-center mt-10"
+            className="text-white flex flex-col items-start justify-center mt-10 bg-gray-800 p-6 rounded-lg shadow-md"
             key={id}
           >
             <div>
-              <div>
-                <h1 className="text-2xl font-serif">
-                  <span className="mr-4 text-2xl">{id}</span>
-                  {question}
-                </h1>
-              </div>
-              <ul className="ml-10">
-                {[A, B, C, D].map((option, index) => {
-                  const optionLetter = ["A", "B", "C", "D"][index];
-                  let optionClass =
-                    "my-2 hover:bg-red-50 hover:outline-none hover:text-green-400 p-2 cursor-pointer rounded-md font-mono";
-
-                  if (questionStatus) {
-                    if (questionStatus.selectedAnswer === option) {
-                      optionClass += questionStatus.isCorrect
-                        ? " bg-green-500 text-white"
-                        : " bg-red-500 text-white";
-                    } else if (questionStatus.correctAnswer === option) {
-                      optionClass += " bg-green-500 text-white";
-                    } else {
-                      optionClass += " opacity-50";
-                    }
-                  }
-
-                  return (
-                    <li
-                      key={optionLetter}
-                      className={optionClass}
-                      onClick={() => ansHandler(id, option, Ans)}
-                    >
-                      <span className="text-2xl">{optionLetter}.</span> {option}
-                    </li>
-                  );
-                })}
-              </ul>
+              <h1 className="text-2xl font-serif mb-4">
+                <span className="mr-4 text-2xl font-bold">{id}</span>
+                {question}
+              </h1>
             </div>
+            <ul className="ml-10">
+              {[A, B, C, D].map((option, index) => {
+                const optionLetter = ["A", "B", "C", "D"][index];
+                let optionClass =
+                  "my-2 hover:bg-gray-200 hover:text-gray-800 p-2 cursor-pointer rounded-md font-mono transition duration-200";
+
+                if (questionStatus) {
+                  if (questionStatus.selectedAnswer === option) {
+                    optionClass += questionStatus.isCorrect
+                      ? " bg-green-500 text-white"
+                      : " bg-red-500 text-white";
+                  } else if (questionStatus.correctAnswer === option) {
+                    optionClass += " bg-green-500 text-white";
+                  } else {
+                    optionClass += " opacity-50";
+                  }
+                }
+
+                return (
+                  <li
+                    key={optionLetter}
+                    className={optionClass}
+                    onClick={() => ansHandler(id, option, Ans)}
+                  >
+                    <span className="text-2xl font-bold">{optionLetter}.</span>{" "}
+                    {option}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         );
       })}
-      <div className="sticky ml-[400px] md:ml-[1000px] bottom-[500px]">
-        <h2 className="text-white bg-red-700 inline-block px-1.5 py-2 rounded-md">
-          Correct Answer: {count}
+      <div className="fixed bottom-10 right-10">
+        <h2 className="text-white bg-red-700 inline-block px-4 py-2 rounded-md">
+          Correct Answers: {count}
         </h2>
       </div>
-    </>
+    </div>
   );
 }
 
